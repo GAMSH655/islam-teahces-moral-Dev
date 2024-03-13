89c1e14e282e0ebe85635e27520e2a107e88e83c
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-03-13 11:46:55 -0700

    working in the mobile view

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -1,26 +1,50 @@
 import '../Header.css'
 import Img from '../assets/itmImg.jpg'
+import { useState } from 'react'
 
 function Header() {
-    
+       const [action , setAction] = useState(false);
+       function handleClick(){
+        console.log("open mobile nav");
+        setAction((action)=>!action);
+       }
     return(
         <>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
          <div className='headerContainer'>
          <img src={Img} alt="" className='logo' />
           <div className='navContainer'>
-              <a href="#">home  <i class="fa-solid fa-house"></i></a>
-              <a href="#">about <i class="fa-solid fa-user"></i></a>
-              <a href="#">activities <i class="fa-solid fa-inbox"></i></a>
+              <a href="#" id='navLink'>home  <i class="fa-solid fa-house"></i></a>
+              <a href="#" id='navLink'>about <i class="fa-solid fa-user"></i></a>
+              <a href="#" id='navLink'>activities <i class="fa-solid fa-inbox"></i></a>
           </div>
-           <div className="buttonContainer">
-              <button id="sigNIN">sign in</button>
-              <button id="signOut">sign in</button>
+           <div className="iconContainer">
+           <i class="fa-brands fa-facebook fa-2xl"></i>
+           <i class="fa-brands fa-instagram fa-2xl"></i>
+           <i class="fa-brands fa-twitter fa-2xl"></i>
            </div>
-           <button className="menuBTn">
+           <button  className="menuBTn" onClick={handleClick}>
            <i class="fa-solid fa-bars fa-2xl "></i>
            </button>
          </div>
+        
+           {/* mobile header */}
+           {action && <div className='mobileNav'>
+           <div className="mobileIconContainer">
+           <i class="fa-brands fa-facebook fa-2xl"></i>
+           <i class="fa-brands fa-instagram fa-2xl"></i>
+           <i class="fa-brands fa-twitter fa-2xl"></i>
+           </div> 
+        
+          <div className='LinkContainer'>
+              <a href="#" id='mobileLink'>home  <i class="fa-solid fa-house"></i></a>
+              <a href="#" id='mobileLink'>about <i class="fa-solid fa-user"></i></a>
+              <a href="#" id='mobileLink'>activities <i class="fa-solid fa-inbox"></i></a>
+          </div>
+         
+
+         </div>/* mobile header */}
+         
         </>
     )
 }
