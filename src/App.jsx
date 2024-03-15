ff982e93d2248ef985e972a3328ad64d6651cbf9
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-03-15 07:58:01 -0700

    in the production

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import Header from './assets/Header'
 import Home from './Home'
 import Mission from './OurMission'
+import Vision from './Vision'
 function App (){
   return(
      <>
        <Header />
        <Home />
        <Mission />
+       <Vision />
      </>
   )
 }export default App
---
+++ src/Vision.jsx
@@ -0,0 +1,20 @@
+import './Vision.css'
+
+
+function VisionComp(){
+     return (
+        <div>
+             <div className="Vision">
+                <img src="./src/assets/katib.png" alt="" />
+                <div className="visionSideTExt">
+                    <h3>MISSION</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex similique quis hic nobis repudiandae fugit odio alias ullam laborum? Obcaecati ex suscipit accusantium, laudantium inventore maiores tenetur reiciendis esse cumque.
+                    Sit quasi beatae amet perspiciatis explicabo magnam, quae aperiam ut quia deserunt. Dicta alias assumenda velit et error maxime necessitatibus saepe fugit. Ea labore sequi quisquam consectetur architecto ducimus nihil.
+                    Adipisci mollitia accusantium tempora eveniet rerum dolorum, alias quam debitis animi eum quibusdam architecto in praesentium numquam ducimus sit ut error perferendis similique nihil dolore. Praesentium doloremque qui fuga modi!
+                    Exercitationem officiis doloremque quasi in voluptatem cumque officia pariatur quis, nulla maxime adipisci rerum, sunt, voluptates laudantium perferendis quae possimus culpa nam! Labore repudiandae non quos dolorem! Quos, autem dolorem!
+                    Enim in, ea expedita, dignissimos consequuntur, corrupti ipsum ab sit molestiae quibusdam possimus! Maiores et odit mollitia quasi iure quibusdam. Magni nam quibusdam maiores totam impedit in vitae pariatur dicta.</p>
+                </div>
+             </div>
+        </div>
+     )
+}export default VisionComp;
