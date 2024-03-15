17bff2d40a5e06a3f8275367a435a6a798ea3ed7
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-03-15 01:27:50 -0700

    working in the header components

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -20,21 +20,20 @@ function Header() {
               <a href="#" id='navLink'>activities <i className="fa-solid fa-inbox"></i></a>
           </div>
            <div className="iconContainer">
-           <i className="fa-brands fa-facebook fa-2xl"></i>
-           <i className="fa-brands fa-instagram fa-2xl"></i>
-           <i className="fa-brands fa-twitter fa-2xl"></i>
+           <a href="https://www.facebook.com/people/Islam-teaches-moral/100076434553906/?mibextid=LQQJ4d"><i className="fa-brands fa-facebook fa-2xl"></i></a>
+            <a href="https://www.instagram.com/islam_teaches_moral?igsh=ajI5cjJvOWlxZ3Uy&utm_source=qr"><i className="fa-brands fa-instagram fa-2xl"></i></a>
+           <a href="  https://twitter.com/itmchannel/"> <i className="fa-brands fa-twitter fa-2xl"></i></a>
            </div>
            <button  className="menuBTn" onClick={handleClick}>
            <i class="fa-solid fa-bars fa-2xl "></i>
            </button>
          </div>
-        
            {/* mobile header */}
            {action && <div className='mobileNav'>
            <div className="mobileIconContainer">
-           <i className="fa-brands fa-facebook fa-2xl"></i>
-           <i className="fa-brands fa-instagram fa-2xl"></i>
-           <i className="fa-brands fa-twitter fa-2xl"></i>
+           <a href="https://www.facebook.com/people/Islam-teaches-moral/100076434553906/?mibextid=LQQJ4d"><i className="fa-brands fa-facebook fa-2xl"></i></a>
+            <a href="https://www.instagram.com/islam_teaches_moral?igsh=ajI5cjJvOWlxZ3Uy&utm_source=qr"><i className="fa-brands fa-instagram fa-2xl"></i></a>
+           <a href="  https://twitter.com/itmchannel/"> <i className="fa-brands fa-twitter fa-2xl"></i></a>
            </div> 
             
           <div className='LinkContainer'>
