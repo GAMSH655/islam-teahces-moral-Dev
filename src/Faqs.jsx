3f4a4b23002d9f0e4ddf85c5d48ff080981a2a79
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-03-19 01:14:31 -0700

    am through working on the accorddion

--- src/Faqs.jsx
+++ src/Faqs.jsx
@@ -2,27 +2,26 @@ import './Faqs.css'
 import { useState } from 'react'
 function FAQS(props){
       
-const [header , setHeader] = useState(false)
-    function handleHeaderClick(){
-         console.log("click")
-    }
-
-
+const [IsOpen , setIsOpen ] = useState(false)
+const ToggleAccordion = () =>{
+  setIsOpen(!IsOpen)
+}
+  
+     
      return(
          <div>
-             {/* <h3 className="FAQSUPPERTEXT">FAQS</h3> */}
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
-             <div>
-             <div className="Faqs" onClick={()=>handleHeaderClick}>
-                 <div className="accordionHeader">
-                   {props.questionAsked}  <i className="fa-solid fa-chevron-up fa-2xl">  </i>
-                 </div>
-                   <div className="answerContainer">
-                      {props.answerText}
-                   </div>
-                </div> 
-             </div>
-                
-         </div> 
+                <div>
+                {/* <h3 className="FAQSUPPERTEXT">FAQS</h3> */}
+               <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+                <div className="Faqs active"  onClick={ToggleAccordion}>
+                    <div className="accordionHeader" >
+                      {props.questionAsked}  <i className="fa-solid fa-chevron-up fa-2xl">  </i>
+                    </div>
+                      <div className = {`answerContainer ${IsOpen ? "active" : ""}`} >
+                       <p>{props.answerText}</p>
+                      </div>
+                   </div> 
+            </div> 
+         </div>
      )
 }export default FAQS
